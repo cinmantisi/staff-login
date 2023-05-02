0e517a29bfe38dc75c9aec58419933d174f5e2f7
cinmantisi <131207869+cinmantisi@users.noreply.github.com>
2023-05-02 18:41:37 +0300

    Add files via upload

---
+++ staff-onay.js
@@ -0,0 +1,63 @@
+const { MessageEmbed } = require('discord.js')
+const cooldown = new Set();
+const mysql = require('mysql');
+const ms = require('ms')
+module.exports = {
+    name : 'staff-login-onay',
+    category : 'info',
+    description : 'Bahis oyunu',
+
+    run : async(client, message, args) => {
+        if(cooldown.has(message.author.id)) {
+            message.reply('error')
+        } else {
+           const kod = args[0]
+           const kullanici = message.author.tag
+           if(!kod) return message.reply('Bir doğrulama kodu belirt.')
+            const habbodb = mysql.createConnection({
+                host: `localhost`,
+                user: `root`,
+                password: `pass`,
+                database: `habbo`
+            })
+
+            habbodb.query(`SELECT * FROM users WHERE dogrulama = '${kod}'` , (err, habbo) => {
+                if(err) {
+                    console.log(err)
+
+                } else if (habbo[0] == undefined) {
+                    message.reply('Böyle bir doğrulama kodu bulamadım.')
+                } else {
+                const embed = new MessageEmbed()
+                .setColor('GREEN')
+                .setTitle('Başarılı')
+                .setDescription('Kullanabileceğiniz komutları görmek için lütfen "staff-help" komutunu kullanın.')
+                .addField('Giriş yapılan hesap', habbo[0].username)
+                message.channel.send(embed)
+                const dogrulamakodu = Math.floor(Math.random() * 1000000)
+                habbodb.query(`UPDATE users SET staffdiscord = '${kullanici}' WHERE dogrulama = '${kod}'`)
+                habbodb.query(`UPDATE users SET dogrulama = '${dogrulamakodu}' WHERE username = '${habbo[0].username}'`)
+                }
+
+
+        })
+
+
+
+
+
+
+
+
+
+
+        cooldown.add(message.author.id);
+        setTimeout(() => {
+            cooldown.delete(message.author.id)
+        }, 10); // here will be the time in miliseconds 5 seconds = 5000 miliseconds
+
+    }
+
+}
+
+}
---
+++ stafflogin.js
@@ -0,0 +1,91 @@
+const { MessageEmbed } = require('discord.js')
+const cooldown = new Set();
+const mysql = require('mysql');
+const nodemailer = require('nodemailer')
+const ms = require('ms')
+module.exports = {
+    name : 'staff-login',
+    category : 'info',
+    description : 'Bahis oyunu',
+
+    run : async(client, message, args) => {
+        if(cooldown.has(message.author.id)) {
+            message.reply('error')
+        } else {  
+            const staff = args[0]
+            const kullanici = message.author.tag
+            if(!staff) return message.reply('Giriş yapılcak bir hesap kullanıcı adı gir.')
+            const habbodb = mysql.createConnection({
+                host: `localhost`,
+                user: `root`,
+                password: `pass`,
+                database: `habbo`
+            })
+
+            var transporter = nodemailer.createTransport({
+              service: 'gmail',
+              auth: {
+                user: 'ulaumeer123@gmail.com',
+                pass: 'ruykjctlfzynazvm'
+              }
+            });
+
+            habbodb.query(`SELECT * FROM users WHERE username = '${staff}'` , (err, habbo) => {
+                if(err) {
+                    console.log(err)
+
+                } else if (habbo[0] == undefined) {
+                    message.channel.send("Böyle bir hesap bulamadım..")
+                } else if (habbo[0].rank < 5) {
+                    message.channel.send('Hmm.. Görünüşe göre bu bir personel hesabı değil.')
+                } else {
+                                     const dogrulamakodu = Math.floor(Math.random() * 1000000)
+                                    const embed = new MessageEmbed()
+                                    .setColor('RED')
+                                    .setTitle('2AD')
+                                    .setDescription('Giriş yapılacak hesabın e-posta adresine gönderilen doğrulama kodunu "i!staff-login-onay <kod>" şeklinde yazınız.')
+                                    message.channel.send(embed)
+                                    habbodb.query(`UPDATE users SET dogrulama = '${dogrulamakodu}' WHERE username = '${staff}'`)
+
+                                    var mailOptions = {
+                                    from: 'example@gmail.com',
+                                    to: `${habbo[0].mail}`,
+                                    subject: `${staff} Adlı personel hesabınız için ${kullanici} adlı hesabın doğrulama kodu : ${dogrulamakodu}`,
+                                    text: 'Kodunuzla girişi onaylayabilirsiniz.'
+                                    }
+
+                                    transporter.sendMail(mailOptions, function(error, info){
+                                        if (error) {
+                                          console.log(error);
+                                        } else {
+                                          console.log('Email sent: ' + info.response);
+                                        }
+
+
+
+
+
+                                    })
+                }
+
+        })
+
+
+
+
+
+
+
+
+
+
+        cooldown.add(message.author.id);
+        setTimeout(() => {
+            cooldown.delete(message.author.id)
+        }, 10); // here will be the time in miliseconds 5 seconds = 5000 miliseconds
+
+    }
+
+}
+
+}
